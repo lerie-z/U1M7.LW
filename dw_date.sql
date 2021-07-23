a255e9810a449b4897ee255e67f387a9993c2830
lerie-z <79635585+lerie-z@users.noreply.github.com>
2021-07-23 20:43:22 +0300

    small fix

--- dw_date.sql
+++ dw_date.sql
@@ -8,7 +8,7 @@ ALTER TABLE u_dw_calendar.dw_date
        DROP CONSTRAINT fk_dw_date_ref_dw_t_month;
 
 ALTER TABLE u_dw_calendar.dw_date 
-       DROP CONSTRAINT fk_dw_date_ref_dw_t_quater;
+       DROP CONSTRAINT fk_dw_date_ref_dw_t_quarter;
 
 ALTER TABLE u_dw_calendar.dw_date 
        DROP CONSTRAINT fk_dw_date_ref_dw_t_year;
@@ -20,13 +20,12 @@ DROP TABLE u_dw_calendar.dw_date CASCADE CONSTRAINTS;
 /*==============================================================*/
 CREATE TABLE u_dw_calendar.dw_date 
 (
-    date_id              NUMBER(4) NOT NULL
-   ,day_id               NUMBER(4)
-   ,week_id              NUMBER(4)
-   ,year_id              NUMBER(4)
-   ,month_id             NUMBER(4)
-   ,quater_id            NUMBER(4)
-   ,date_full_string     VARCHAR2 ( 20 CHAR )
+    date_id              NUMBER(22,0)          NOT NULL
+   ,day_id               NUMBER(22,0)
+   ,week_id              NUMBER(22,0)
+   ,year_id              NUMBER(22,0)
+   ,month_id             NUMBER(22,0)
+   ,quarter_id           NUMBER(22,0)
    ,CONSTRAINT "pk_u_dw_calendar.dw_date" PRIMARY KEY (date_id)
 );
 
@@ -36,9 +35,10 @@ CREATE TABLE u_dw_calendar.dw_date
 CREATE TABLE u_dw_calendar.t_days 
 (
     day_id               NUMBER(22,0)          NOT NULL
-   ,day_full_string      VARCHAR(30)
-   ,day_of_week          VARCHAR(30)
-   ,day_num_of_year      NUMBER(22,0)
+   ,day_name             VARCHAR(30)
+   ,day_number_week      NUMBER(1)
+   ,day_number_month     NUMBER(2)
+   ,day_number_year      NUMBER(3)
    ,CONSTRAINT "pk_u_dw_calendar.t_days" PRIMARY KEY (day_id)
 );
 
@@ -47,23 +47,23 @@ CREATE TABLE u_dw_calendar.t_days
 /*==============================================================*/
 CREATE TABLE u_dw_calendar.t_weeks 
 (
-    week_id              NUMBER(4)          NOT NULL
-   ,week_num_of_year     NUMBER(4)
-   ,week_starts_dt       DATE
-   ,week_ends_dt         DATE
+    week_id              NUMBER(22,0)          NOT NULL
+   ,week_number          NUMBER(2)
+   ,week_end_dt          DATE
    ,CONSTRAINT "pk_u_dw_calendar.t_weeks" PRIMARY KEY (week_id)
 );
 
 /*==============================================================*/
-/* Table: u_dw_calendar.t_quater                                       */
+/* Table: u_dw_calendar.t_quaRter                                       */
 /*==============================================================*/
-CREATE TABLE u_dw_calendar.t_quater 
+CREATE TABLE u_dw_calendar.t_quarter 
 (
-    quater_id               NUMBER(4)  NOT NULL
-   ,quater_starts_dt        DATE
-   ,quater_ends_dt          DATE
-   ,quater_days_cnt         NUMBER(4)
-   ,CONSTRAINT "pk_u_dw_calendar.t_quater" PRIMARY KEY (quater_id)
+    quarter_id            NUMBER(22,0)          NOT NULL
+   ,quarter_num           NUMBER(1)             NOT NULL
+   ,quarter_days_cnt      NUMBER(3)
+   ,quarter_starts_dt     DATE
+   ,quarter_ends_dt       DATE
+   ,CONSTRAINT "pk_u_dw_calendar.t_quarter" PRIMARY KEY (quarter_id)
 );
 
 /*==============================================================*/
@@ -71,10 +71,10 @@ CREATE TABLE u_dw_calendar.t_quater
 /*==============================================================*/
 CREATE TABLE u_dw_calendar.t_month 
 (
-    month_id             NUMBER(4)          NOT NULL
-   ,month_full_string    VARCHAR(30)
-   ,month_num            NUMBER(4) 
-   ,month_day_cnt        NUMBER(4)
+    month_id             NUMBER(22,0)          NOT NULL
+   ,month_number         NUMBER(2)
+   ,month_name           VARCHAR(30)
+   ,month_days_cnt       NUMBER(3)
    ,CONSTRAINT "pk_u_dw_calendar.t_month" PRIMARY KEY (month_id)
 );
 
@@ -85,9 +85,7 @@ CREATE TABLE u_dw_calendar.t_years
 (
     year_id              NUMBER(22,0)          NOT NULL
    ,year_calendar        NUMBER(4)
-   ,YEAR_DAYS_CNT        NUMBER(4)
-   ,year_starts_dt       DATE
-   ,year_ends_dt         DATE
+   ,year_days_cnt        NUMBER(3)
    ,CONSTRAINT "pk_u_dw_calendar.t_years" PRIMARY KEY (year_id)
 );
 
@@ -103,12 +101,10 @@ ALTER TABLE u_dw_calendar.dw_date
    ADD CONSTRAINT fk_dw_date_ref_dw_t_month FOREIGN KEY (month_id)
       REFERENCES u_dw_calendar.t_month (month_id);
 
-
 ALTER TABLE u_dw_calendar.dw_date
    ADD CONSTRAINT fk_dw_date_ref_dw_t_year FOREIGN KEY (year_id)
       REFERENCES u_dw_calendar.t_years (year_id);      
       
 ALTER TABLE u_dw_calendar.dw_date
-   ADD CONSTRAINT fk_dw_date_ref_dw_t_quater FOREIGN KEY (quater_id)
-      REFERENCES u_dw_calendar.t_quater (quater_id);
-
+   ADD CONSTRAINT fk_dw_date_ref_dw_t_quarter FOREIGN KEY (quarter_id)
+      REFERENCES u_dw_calendar.t_quarter (quarter_id);
